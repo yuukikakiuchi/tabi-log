01e684df9ab291fdd67c31c5b38f9a8c9b7438c0
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-07-09 21:14:42 +0900

    replaced the message in view
    login edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  resources :messages, only: :index
+  resources :posts, only: :index
+  resources :users, only: :index
+  # get 'messages/index'
   root "messages#index"
-  
+  # get 'posts/index'
 end
