34b6505709c1dca20c35e884bb39e80e298713c9
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-08-27 18:08:28 +0900

    mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2020_07_16_014321) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.text "check_1"
-    t.bigint "check1_id"
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_category_relations", id: :bigint, default: -> { "nextval('post_categories_id_seq'::regclass)" }, force: :cascade do |t|
-    t.bigint "post_id"
-    t.bigint "category_id"
+  create_table "checks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_prefecture_relations", force: :cascade do |t|
+  create_table "post_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_prefecture_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "score"
     t.bigint "post_id"
@@ -36,35 +38,26 @@ ActiveRecord::Schema.define(version: 2020_07_16_014321) do
     t.index ["prefecture_id"], name: "index_post_prefecture_relations_on_prefecture_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.string "image"
-    t.text "areaname"
-    t.text "large_area"
-    t.text "medium_area"
-    t.text "check_1"
-    t.text "check_2"
+    t.string "areaname"
+    t.string "large_area"
+    t.string "medium_area"
+    t.string "check_1"
+    t.string "check_2"
     t.string "check_3"
-    t.text "genre"
-    t.text "check_4"
-    t.text "check_5"
-    t.text "check_6"
-    t.text "check_7"
-    t.text "check_8"
-    t.text "check_9"
   end
 
-  create_table "prefectures", force: :cascade do |t|
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name"
     t.string "region_id"
   end
 
-  create_table "regions", force: :cascade do |t|
+  create_table "regions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name"
   end
 
-  add_foreign_key "post_category_relations", "categories", name: "post_category_relations_category_id_fkey"
-  add_foreign_key "post_category_relations", "posts", name: "post_category_relations_post_id_fkey"
   add_foreign_key "post_prefecture_relations", "posts"
   add_foreign_key "post_prefecture_relations", "prefectures"
 end
