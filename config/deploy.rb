b6ac3b90d9c24a5fd3953cb9f3bd2f81c5e81969
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-08-28 19:39:59 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 set :application, 'tabi-log'
-set :repo_url, 'git@example.com:yuukikakiuchi/tabi-log.git'
+set :repo_url, 'git@github.com:yuukikakiuchi/tabi-log.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
