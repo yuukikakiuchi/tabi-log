fd18d9192708bd9830728f76e38102e25879ff97
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-08-31 17:47:51 +0900

    1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 
-// require("jquery_ujs")
+require('jquery_ujs')
 //= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
