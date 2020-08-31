12460d8d3217044c0674b627b0a47929b2da5459
yuukikakiuchi <ccb64890.yu@icloud.com>
2020-08-31 17:32:39 +0900

    1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require'../image_preview'
+require('../image_preview')
 // require("jquery_ujs")
 //= require_tree .
 
